1af00e3f5d04a5a550044717de2f06f26922344c
LPR7124 <liviapr@al.insper.edu.br>
2026-07-26 19:51:03 -0300

    questao_6 revisada

---
+++ questao_6.py
@@ -0,0 +1,19 @@
+from questao_1 import *
+from questao_2 import *
+from questao_3 import *
+from questao_4 import *
+from questao_5 import *
+
+def questao_para_texto (questao, id):
+    linhas = f'----------------------------------------\n'
+    primeira = f"QUESTAO {id}\n"
+    segunda = f'\n'
+    terceira = f'{questao["titulo"]}\n'
+    quarta = f'\n'
+    quinta = f'RESPOSTAS:\n'
+    sexta = f'A: {questao["opcoes"]["A"]}\n'
+    setima = f'B: {questao["opcoes"]["B"]}\n'
+    oitava = f'C: {questao["opcoes"]["C"]}\n'   
+    nona = f'D: {questao["opcoes"]["D"]}\n'
+
+    return linhas + primeira + segunda + terceira + quarta + quinta + sexta + setima + oitava + nona
