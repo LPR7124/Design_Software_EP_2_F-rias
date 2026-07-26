b85f2d9fd33a84ae383feab001135d59dbfb64ab
LPR7124 <liviapr@al.insper.edu.br>
2026-07-26 17:43:54 -0300

    questao_4 verificada

---
+++ questao_4.py
@@ -0,0 +1,15 @@
+from questao_1 import *
+from questao_2 import *
+from questao_3 import *
+
+import random
+def sorteia_questao (questoes_nivel, nivel):
+    # Pego no dic de questões um nivel dado
+    lista_nivel = questoes_nivel[nivel]
+
+    #Uso a função randint para fazer o sorteio : sorteia um indice aleatório 
+    indice_sorteado = random.randint(0, len(lista_nivel) - 1)
+
+    #usa o índice aleatório sorteado 
+    questao_sorteada = lista_nivel[indice_sorteado]
+    return questao_sorteada
