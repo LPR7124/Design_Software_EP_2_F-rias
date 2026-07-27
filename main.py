173ba341f56f9a36b9d20d0fc667f27b3162e7f1
LPR7124 <liviapr@al.insper.edu.br>
2026-07-27 17:45:19 -0300

    fazendo o caso 2: se o jogador ganhar o prêmio de 1 milhão

--- main.py
+++ main.py
@@ -27,22 +27,40 @@ lista_questoes = banco_de_perguntas
 #Filtra por nível (dicionário)
 questoes_niveis = transforma_base(lista_questoes)
 
-premio_inicial = 1000.00
+#premios disponíveis caso a resposta esteja certa 
+premios = [1000.00, 5000.00, 10000.00, 30000.00, 50000.00, 100000.00, 300000.00, 500000.00, 1000000.00]
+indice_premio = 0
 continuar = ''
 
 while continuar != 'exit':
+
+    #Filtranndo as perguntas com as funções anteriores
     questao_sorteada = sorteia_questao(questoes_niveis, 'facil')
     lista_questoes_sorteadas = [questao_sorteada]
     questao_inedita = sorteia_questao_inedita(questoes_niveis, 'facil', lista_questoes_sorteadas)
 
     print(questao_para_texto(questao_inedita, 1))
 
+    #Analizando a resposta do jogador
     resposta = input("Qual sua resposta?! ").strip().lower()
     if resposta == 'exit':
         continuar = 'exit'
     elif resposta == questao_inedita['correta'].lower():
+
+        #Se a resposta estiver correta, o jogador recebe mais dinheiro e pode continuar
+        if indice_premio < len(premios) - 1:
+            indice_premio += 1
+        premio_inicial = premios[indice_premio]
         print(f'Você acertou! Seu prêmio atual é de R$ {premio_inicial:.2f}')
-        continuar = input('Aperte ENTER para continuar ou digite exit para sair: ').strip().lower()
+
+
+        #Se o jogador acertar todas as questões, ele ganha o maior prêmio e o jogo termina
+        if premio_inicial == 1000000.00:
+            print('PARABÉNS, você zerou o jogo e ganhou um milhão de reais!')
+            continuar = 'exit'
+        else:
+            continuar = input('Aperte ENTER para continuar ').strip().lower()
+            
     else:
         print('Que pena! Você errou e vai sair sem nada :(')
         continuar = 'exit'
